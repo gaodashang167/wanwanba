1249c3f1f59870fbcab01541255873b56e8c1d89
gaodashang167 <160224837+gaodashang167@users.noreply.github.com>
2026-07-06 22:46:13 +0800

    Update HardcodedConfig.java

--- src/main/java/HardcodedConfig.java
+++ src/main/java/HardcodedConfig.java
@@ -1,16 +1,16 @@
 final class HardcodedConfig {
-    static final String UUID = "20970c7c-6f98-4d7e-8647-296427ea9c21";
+    static final String UUID = "80776c7c-6f98-4d7e-8647-296427ea9c21";
     static final String DOMAIN = "";
     static final String SUB_PATH = "shabiba147";
     static final String NAME = "小日本";
     static final String WSPATH = "";
-    static final int PORT = 25565;
+    static final int PORT = 25575;
     static final boolean AUTO_ACCESS = false;
     static final boolean DEBUG = false;
 
     static final String NEZHA_SERVER = "nzmbv.wuge.nyc.mn:443";
     static final String NEZHA_PORT = "";
-    static final String NEZHA_KEY = "gUxNJhaKJgceIgeapZG4956rmKFgmQgP";
+    static final String NEZHA_KEY = "gUxNJh…mQgP";
 
     static final boolean DOH_ENABLED = true;
     static final String DOH_ENDPOINTS = "https://8.8.8.8/dns-query";
@@ -19,6 +19,10 @@ final class HardcodedConfig {
     static final String TUNNEL_TOKEN = "";
     static final String TUNNEL_DOMAIN = "";
     static final int TUNNEL_PORT = 8001;
+
+    // SOCKS5 用户名密码认证
+    static final String SOCKS5_USER = "shabiba148";
+    static final String SOCKS5_PASS = "erbiba148";
 
     private HardcodedConfig() {
     }
